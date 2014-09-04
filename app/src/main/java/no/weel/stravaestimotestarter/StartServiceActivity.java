d1992324605aaaff7bc63435ff142b84a32d4977
Morten Weel Johnsen <mweeljoh@cisco.com>
2014-09-04 14:04:56 +0200

    Add possibility to start service manually from launcher.

---
+++ app/src/main/java/no/weel/stravaestimotestarter/StartServiceActivity.java
@@ -0,0 +1,17 @@
+package no.weel.stravaestimotestarter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class StartServiceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startService(new Intent(this, EstimoteService.class));
+    }
+}
