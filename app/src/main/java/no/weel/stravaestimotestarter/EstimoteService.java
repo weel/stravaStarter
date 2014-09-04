8f7b6b37a8d0c58b32b81cc564203ee3dba33090
Morten Weel Johnsen <weel@Mortens-MacBook-Pro.local>
2014-09-04 22:30:37 +0200

    Fixed problem with URI for stopping intent

--- app/src/main/java/no/weel/stravaestimotestarter/EstimoteService.java
+++ app/src/main/java/no/weel/stravaestimotestarter/EstimoteService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.estimote.sdk.Beacon;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class EstimoteService extends Service {
+    private static final String LOGTAG = "EstimoteService";
     private static BeaconManager beaconManager;
     private static final Region ALL_ESTIMOTE_BEACONS = new Region("regionId",
             "B9407F30-F5F8-466E-AFF9-25556B57FE6D", null, null);
@@ -25,24 +27,28 @@ public class EstimoteService extends Service {
     }
 
     @Override
-    public int onStartCommand(final Intent intent, final int flags, final int startId) {
-
+    public void onCreate() {
+        super.onCreate();
         beaconManager = new BeaconManager(getApplicationContext());
         beaconManager.setBackgroundScanPeriod(TimeUnit.SECONDS.toMillis(1), 0);
         beaconManager.setMonitoringListener(new MonitoringListener() {
             @Override
             public void onEnteredRegion(Region region, List<Beacon> beacons) {
+                Toast.makeText(getApplicationContext(), "Entered bike! Enjoy the ride!", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(Intent.ACTION_RUN);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.setData(Uri.parse("http://strava.com/nfc/record"));
+                intent.putExtra("data", "http://strava.com/nfc/record");
                 startActivity(intent);
             }
 
             @Override
             public void onExitedRegion(Region region) {
+
+                Toast.makeText(getApplicationContext(), "Exited bike! Hope you enjoyed the ride!", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(Intent.ACTION_RUN);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.setData(Uri.parse("http://strava.com/nfc/stop"));
+                intent.setData(Uri.parse("http://strava.com/nfc/record/stop"));
                 startActivity(intent);
             }
         });
@@ -55,13 +61,12 @@ public class EstimoteService extends Service {
                     // ... and start the monitoring
                     beaconManager.startMonitoring(ALL_ESTIMOTE_BEACONS);
                 } catch (Exception e) {
+                    Log.e(LOGTAG, "Got exception in monitoring" + e);
                 }
             }
         });
 
-        return START_STICKY;
     }
-
 
     @Override
     public void onDestroy() {
--- app/src/main/java/no/weel/stravaestimotestarter/StartServiceActivity.java
+++ app/src/main/java/no/weel/stravaestimotestarter/StartServiceActivity.java
@@ -2,7 +2,9 @@ package no.weel.stravaestimotestarter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +14,7 @@ public class StartServiceActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         startService(new Intent(this, EstimoteService.class));
     }
 }
